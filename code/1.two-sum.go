bba2d4e5233cbba150ab8eaa05b13bb9d00f0947
v8yte <buihachi.nishi@gmail.com>
2024-01-10 23:45:14 +0900

    1

--- code/1.two-sum.go
+++ code/1.two-sum.go
@@ -1,13 +1,13 @@
 func twoSum(nums []int, target int) []int {
-    m := make(map[int]int,0)
-    for i,n := range nums {
-        m[n] = i
-    }
-
-    for i,v := range nums {
-        if k,ok := m[target - v]; ok && i != k {
-            return []int{i,k}
+    // target - num が辞書にあるか
+    // 辞書に突っ込む O(n)
+    numsMap := make(map[int]int,len(nums)-1)
+    
+    for i, num := range nums {
+        if idx,ok := numsMap[target - num]; ok {
+            return []int{i,idx}
         }
+        numsMap[num] = i
     }
     return []int{}
 }
